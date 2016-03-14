a34086213ea8730b4b9a8f3e656cd1abeebad0d7
m4b <m4b.github.io@gmail.com>
2016-03-13 23:28:28 -0700

    remove more extraneous files

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "Building musl linked binary"
-gcc -nodefaultlibs -nostdlib -Wl,-I/tmp/dryad.so.1 -L/home/m4b/src/musl-1.1.14/lib -lc /home/m4b/src/musl-1.1.14/lib/crt1.o test/musl.c -o musl
-
-export LD_LIBRARY_PATH=/home/m4b/src/musl-1.1.14/lib
-echo -e "running musl binary with $LD_LIBRARY_PATH"
-./musl
-
