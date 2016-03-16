0d0bbe4ad542c7539129df48c38c02174de34b50
m4b <m4b.github.io@gmail.com>
2016-03-15 23:23:39 -0700

    docs, build: update readme about new faster build, and fix too much output and remove sudo

--- make_static.sh
+++ make_static.sh
@@ -7,9 +7,9 @@ PREFIX=$(pwd)/musldist
 curl -O http://www.musl-libc.org/releases/musl-1.1.14.tar.gz
 tar xf musl-1.1.14.tar.gz
 cd musl-1.1.14/
-CFLAGS=-fPIC ./configure --disable-shared --prefix=$PREFIX
-make
-make install
+CFLAGS=-fPIC ./configure --disable-shared --prefix=$PREFIX &> /dev/null
+make &> /dev/null
+make install &> /dev/null
 cd ..
 echo -e "Installing and building nightly rustc compiler with musl target"
 mkdir muslrust
@@ -17,4 +17,4 @@ cd muslrust
 # hack
 curl -sSf https://static.rust-lang.org/rustup.sh > rustup.sh
 chmod +x rustup.sh
-./rustup.sh --channel=nightly --with-target=x86_64-unknown-linux-musl --prefix=$PREFIX 2&> /dev/null
+./rustup.sh --disable-sudo --channel=nightly --with-target=x86_64-unknown-linux-musl --prefix=$PREFIX
