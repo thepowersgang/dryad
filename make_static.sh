e592552f7065147a77a0ff069b0c879d3457dae3
m4b <m4b.github.io@gmail.com>
2016-03-15 23:17:39 -0700

    remove rustc download from make_static and add rustup

--- make_static.sh
+++ make_static.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+echo -e "Installing and building musl"
 mkdir musldist
 PREFIX=$(pwd)/musldist
 # Build musl
@@ -10,24 +11,10 @@ CFLAGS=-fPIC ./configure --disable-shared --prefix=$PREFIX
 make
 make install
 cd ..
-# Build libunwind.a
-curl -O http://llvm.org/releases/3.7.0/llvm-3.7.0.src.tar.xz
-tar xf llvm-3.7.0.src.tar.xz
-cd llvm-3.7.0.src/projects/
-curl http://llvm.org/releases/3.7.0/libcxxabi-3.7.0.src.tar.xz | tar xJf -
-mv libcxxabi-3.7.0.src libcxxabi
-curl http://llvm.org/releases/3.7.0/libunwind-3.7.0.src.tar.xz | tar xJf -
-mv libunwind-3.7.0.src libunwind
-mkdir libunwind/build
-cd libunwind/build
-cmake -DLLVM_PATH=../../.. -DLIBUNWIND_ENABLE_SHARED=0 ..
-make
-cp lib/libunwind.a $PREFIX/lib/
-cd ../../../../
-# Build musl-enabled rust
-git clone https://github.com/rust-lang/rust.git muslrust
+echo -e "Installing and building nightly rustc compiler with musl target"
+mkdir muslrust
 cd muslrust
-./configure --target=x86_64-unknown-linux-musl --musl-root=$PREFIX --prefix=$PREFIX
-make
-make install
-cd ..
+# hack
+curl -sSf https://static.rust-lang.org/rustup.sh > rustup.sh
+chmod +x rustup.sh
+./rustup.sh --channel=nightly --with-target=x86_64-unknown-linux-musl --prefix=$PREFIX 2&> /dev/null
